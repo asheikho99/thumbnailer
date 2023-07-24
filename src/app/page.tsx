b7a1fe8f703e6fdbd5affcac6c8016b5667c23b8
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-07-24 13:35:38 -0300

    Site content container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,44 @@
 import SearchBar from "@/components/SearchBar";
 import Thumbnail from "@/components/Thumbnail";
 
-export default async function RootPage({ params, searchParams }: { params: string; searchParams: string }) {
+export default async function RootPage({
+  params,
+  searchParams,
+}: {
+  params: string;
+  searchParams: string;
+}) {
   const { search } = searchParams;
   let videoThumbnails;
 
   if (search) {
     const res = await fetch(
-      `https://www.googleapis.com/youtube/v3/videos?key=${process.env.API_KEY}&part=snippet&id=${search}`
+      `https://www.googleapis.com/youtube/v3/videos?key=${process.env.API_KEY}&part=snippet&id=${search}`,
     );
     const videoData: VideosList = await res.json();
     videoThumbnails = videoData.items[0].snippet.thumbnails;
   }
 
   return (
-    <main className="min-h-full p-4">
-      <SearchBar />
-      {videoThumbnails &&
-        Object.entries(videoThumbnails).map((thumbnail) => {
-          const title = thumbnail[0];
-          const data = thumbnail[1];
-          return (
-            <Thumbnail
-              key={title}
-              title={title}
-              alt={data.alt}
-              src={data.url}
-              height={data.height}
-              width={data.width}
-            />
-          );
-        })}
+    <main className="flex min-h-full flex-col">
+      <div className="container space-y-4">
+        <SearchBar />
+        {videoThumbnails &&
+          Object.entries(videoThumbnails).map((thumbnail) => {
+            const title = thumbnail[0];
+            const data = thumbnail[1];
+            return (
+              <Thumbnail
+                key={title}
+                title={title}
+                alt={data.alt}
+                src={data.url}
+                height={data.height}
+                width={data.width}
+              />
+            );
+          })}
+      </div>
     </main>
   );
 }
